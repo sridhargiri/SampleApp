49ac2d1643fc2e97cd44e04ef109e607da4470b4
Sridhar S <sridhargiri1@gmail.com>
2020-10-05 00:13:56 +0530

    404

--- Tania/router-example/src/App.js
+++ Tania/router-example/src/App.js
@@ -6,13 +6,22 @@ import Add from './pages/Add'
 import View from './pages/View'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
+const NoMatchPage = () => {
+  return (
+  	 <div className="container">
+  	  <div className="row">
+    <h3>This page not found (404 error)</h3>
+    </div>
+    </div>
+  );
+};
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={UserPage} />
       <Route path="/Add" component={Add} />
       <Route path="/view/:id" component={View} />   
+      <Route component={NoMatchPage} />
     </Switch>
   )
 }
